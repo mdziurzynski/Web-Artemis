d253aed599ded956fd8c4f7076760d8dfe30db07
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-06-10 15:49:02 +0000

    abbreviate featureDisplay to fDisplay
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/javascript/web-artemis@14001 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- js/scrolling.js
+++ js/scrolling.js
@@ -5,72 +5,71 @@ var scrolling = 0;
 var timeoutTime = 60; // ms
 var lastLeftBase = -1;
 
-function scroll(direction, featureDisplay) {
+function scroll(direction, fDisplay) {
     if (scrolling == 1) {
     	
-    	var leftBase = $('#slider'+featureDisplay.index).slider('option', 'value');
+    	var leftBase = $('#slider'+fDisplay.index).slider('option', 'value');
             if(direction == 1) {
             	// scroll right
             	leftBase = Math.round(Math.floor(
-            			leftBase + $('#slider'+featureDisplay.index).slider('option', 'step')/4));
+            			leftBase + $('#slider'+fDisplay.index).slider('option', 'step')/4));
             }
             else {
             	// scroll left
             	leftBase = Math.round(Math.floor(
-            			leftBase - $('#slider'+featureDisplay.index).slider('option', 'step')/4));
+            			leftBase - $('#slider'+fDisplay.index).slider('option', 'step')/4));
             	if(leftBase == 0) {
             		leftBase = 1;
             	}
             }
             
-        	if(leftBase < 1 || leftBase > sequenceLength) {
+        	if(leftBase < 1 || leftBase > fDisplay.sequenceLength) {
         		return false;
         	}
         	
-        	featureDisplay.leftBase = leftBase;
-        	drawAll(featureDisplay);
-        	$('#slider'+featureDisplay.index).slider('option', 'value', leftBase);
+        	fDisplay.leftBase = leftBase;
+        	drawAll(fDisplay);
+        	$('#slider'+fDisplay.index).slider('option', 'value', leftBase);
 
         	//var code2run = "scroll("+direction+"," +"featureDisplay)";
-            setTimeout(function() { scroll(direction, featureDisplay); }, timeoutTime);
+            setTimeout(function() { scroll(direction, fDisplay); }, timeoutTime);
 
     }
     return false;
 }
 
-function addScrollEventHandlers(featureDisplay) {
-
-	$('#left'+featureDisplay.index).mousedown(function(event){
+function addScrollEventHandlers(fDisplay) {
+	$('#left'+fDisplay.index).mousedown(function(event){
 		if(scrolling != 1) {
-			lastLeftBase = $('#slider'+featureDisplay.index).slider('option', 'value');
+			lastLeftBase = $('#slider'+fDisplay.index).slider('option', 'value');
 		}
 		scrolling = 1;
-		featureDisplay.minimumDisplay = true
-		scroll(-1, featureDisplay);
+		fDisplay.minimumDisplay = true
+		scroll(-1, fDisplay);
 	});
 
-	$('#left'+featureDisplay.index).mouseup(function(event){
+	$('#left'+fDisplay.index).mouseup(function(event){
 		scrolling = 0;
-		featureDisplay.minimumDisplay = false
-		var leftBase = $('#slider'+featureDisplay.index).slider('option', 'value');
-    	featureDisplay.leftBase = leftBase;
-    	drawAndScroll(featureDisplay, lastLeftBase);
+		fDisplay.minimumDisplay = false
+		var leftBase = $('#slider'+fDisplay.index).slider('option', 'value');
+		fDisplay.leftBase = leftBase;
+    	drawAndScroll(fDisplay, lastLeftBase);
 	});
 	
-	$('#right'+featureDisplay.index).mousedown(function(event){
+	$('#right'+fDisplay.index).mousedown(function(event){
 		if(scrolling != 1) {
-			lastLeftBase = $('#slider'+featureDisplay.index).slider('option', 'value');
+			lastLeftBase = $('#slider'+fDisplay.index).slider('option', 'value');
 		}
 		scrolling = 1;
-		featureDisplay.minimumDisplay = true
-		scroll(1, featureDisplay);
+		fDisplay.minimumDisplay = true
+		scroll(1, fDisplay);
 	});
 	
-	$('#right'+featureDisplay.index).mouseup(function(event){
+	$('#right'+fDisplay.index).mouseup(function(event){
 		scrolling = 0;
-		featureDisplay.minimumDisplay = false;
-		var leftBase = $('#slider'+featureDisplay.index).slider('option', 'value');
-		featureDisplay.leftBase = leftBase;
-		drawAndScroll(featureDisplay, lastLeftBase);
+		fDisplay.minimumDisplay = false;
+		var leftBase = $('#slider'+fDisplay.index).slider('option', 'value');
+		fDisplay.leftBase = leftBase;
+		drawAndScroll(fDisplay, lastLeftBase);
 	});
 }
