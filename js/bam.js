f5e7b6ff09069d8c768895adffe4c2029b054069
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-07-08 15:50:04 +0000

    draw coverage plots when zoomed out
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/javascript/web-artemis@14183 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- js/bam.js
+++ js/bam.js
@@ -1,5 +1,5 @@
 
-var maxBamHgt = 220;
+var maxBamHgt = 150;
 
 function bamObj(fDisplay) {
 	drawBam(fDisplay);
@@ -21,10 +21,43 @@ var aSamSeqs = function ajaxGetSamSeqs(fDisplay, returned, options) {
 	var start = fDisplay.leftBase;
 	var end = start + fDisplay.basesDisplayWidth;
 	
-	var serviceName = '/sams/query.json?';
-	handleAjaxCalling(serviceName, aSamCall,
+	if(fDisplay.basesDisplayWidth > 4000) {
+		var step = Math.round(fDisplay.basesDisplayWidth/100);
+		var serviceName = '/sams/coverage.json?';
+		handleAjaxCalling(serviceName, aSamCoverage,
+			{ fileID:1, sequence:sequenceName, start:start, end:end, step:step }, fDisplay, { step:step });
+	} else {
+		var serviceName = '/sams/query.json?';
+		handleAjaxCalling(serviceName, aSamCall,
 			{ fileID:1, sequence:sequenceName, start:start, end:end }, fDisplay, { });
+	}
 };
+
+var aSamCoverage = function ajaxGetSamCoverage(fDisplay, returned, options) {
+	var coverage = returned.response.coverage.counts;
+	var step = options.step;
+	baseInterval = (fDisplay.basesDisplayWidth/displayWidth)*screenInterval;
+	var basePerPixel  = baseInterval/screenInterval;
+	var max = -100;
+	
+	for(var i=0; i<coverage.length; i++ ) {
+		debugLog(coverage[i]);
+		if(coverage[i]>max) {
+			max = coverage[i];
+		}
+	}
+	
+	for(var i=0; i<coverage.length-1; i++ ) {
+		
+		var xpos1 = margin+Math.round( ((i*step)+(step/2)) /basePerPixel);
+		var ypos1 = fDisplay.marginTop-((coverage[i]/max)*maxBamHgt);
+		var xpos2 = margin+Math.round( (((i+1)*step)+(step/2)) /basePerPixel);
+		var ypos2 = fDisplay.marginTop-((coverage[i+1]/max)*maxBamHgt);
+
+		$("#bam"+fDisplay.index).drawLine(xpos1, ypos1, xpos2, ypos2,
+				{color:colour, stroke:'1'});
+	}
+}
 
 var aSamCall = function ajaxGetSamRecords(fDisplay, returned, options) {
 	var samRecords  = returned.response.records;
@@ -38,34 +71,43 @@ var aSamCall = function ajaxGetSamRecords(fDisplay, returned, options) {
 	var ypos  = fDisplay.marginTop-10;
 	
     var lastEndAtZero = -100;
+    var lastStart = -1;
+    var lastEnd   = -1; 
     var properPair = true;
     
+    var colour = '#000000';
 	for(var i=0; i<samRecords.alignmentStart.length; i++ ) {
-
 		var thisStart = alignmentStart[i]-fDisplay.leftBase;
 		var thisEnd   = alignmentEnd[i]-fDisplay.leftBase;
 		var thisName  = name[i];
 		var thisFlags = flags[i];
 		
-		var colour = '#000000';
-		// proper pair
-		if(thisFlags & 0x0002) {
-			colour = '#0000FF';
-		} else if(properPair && !(thisFlags & 0x0002)) {
-			continue;
-		}
-		
-		thisStart = margin+Math.round(thisStart/basePerPixel);
-		thisEnd   = margin+Math.round(thisEnd/basePerPixel);
-		
-		if(thisStart > lastEndAtZero+1 || (fDisplay.marginTop-ypos) > maxBamHgt) {
-			ypos=fDisplay.marginTop-10;
-			lastEndAtZero = thisEnd;
+		if(lastStart == thisStart && lastEnd == thisEnd) {
+			if(colour == '#32cd32') {
+				// already drawn
+				continue;
+			}
+			colour = '#32cd32';
 		} else {
-			ypos=ypos-1;
+			lastStart = thisStart;
+			lastEnd   = thisEnd;
+			
+			if(thisFlags & 0x0002) {
+				colour = '#0000FF';
+			} else if(properPair && !(thisFlags & 0x0002)) {
+				continue;
+			}
+			if(thisStart > lastEndAtZero+1 || (fDisplay.marginTop-ypos) > maxBamHgt) {
+				ypos=fDisplay.marginTop-10;
+				lastEndAtZero = thisEnd;
+			} else {
+				ypos=ypos-1.5;
+			}
 		}
-		
+			
+		thisStart = margin+Math.round(thisStart/basePerPixel);
+		thisEnd   = margin+Math.round(thisEnd/basePerPixel);	
 		$("#bam"+fDisplay.index).drawLine(thisStart, ypos, thisEnd, ypos,
 				{color:colour, stroke:'1'});
 	}
-};
+}
