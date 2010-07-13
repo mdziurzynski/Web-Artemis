9b3f89950812efa4d0ff91887d7311215d96cf91
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-07-13 11:15:11 +0000

    manage zooming
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/javascript/web-artemis@14207 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ js/zoom.js
@@ -0,0 +1,79 @@
+//
+// ZOOMING
+//
+var zooming = 0;
+var zoomtimeoutTime = 450; // ms
+
+function setScrollHandle(scrollbar, fDisplay) {	
+	var slider = $('#slider'+fDisplay.index);
+	slider.slider('option', 'step', fDisplay.basesDisplayWidth/2);
+
+	// TODO slider scaling
+	// http://groups.google.com/group/jquery-ui/browse_thread/thread/1605420a9af60ab2
+	//
+	/*var handleSize = fDisplay.basesDisplayWidth/fDisplay.sequenceLength*100;
+	
+	if(handleSize > 5) {
+		handleSize = 5;
+	}
+	//debugLog("SLIDER WIDTH: "+scrollbar.find('.ui-slider-handle').css('width'));
+	scrollbar.find('.ui-slider-handle').css({ width: handleSize+'%' });*/
+}
+
+function zoom(fDisplay, scrollbar, direction) {
+    if (zooming > 0) {
+    	var step = Math.round(fDisplay.basesDisplayWidth/2)*direction;
+    	var value = $('#slider_vertical_container'+fDisplay.index).slider('option', 'value')+step;
+		if(value > fDisplay.sequenceLength-140) {
+			value = fDisplay.sequenceLength-140;
+		}
+
+	    $('#slider_vertical_container'+fDisplay.index).slider( "option", "value", value );
+	    
+    	zoomOnce(fDisplay, scrollbar);	
+    	setTimeout(function() { zoom(fDisplay, step, direction); }, zoomtimeoutTime);  
+    }
+    return false;
+}
+
+function zoomOnce(fDisplay, scrollbar) {
+    var value = $('#slider_vertical_container'+fDisplay.index).slider('option', 'value');
+    var basesInView = fDisplay.sequenceLength-value;
+    if(basesInView > 50000) {
+    	  showStopCodons = false;
+    } else if(basesInView < 1000) {
+    	  showStopCodons = true;
+    }
+    	  
+    fDisplay.basesDisplayWidth = basesInView;
+    drawAll(fDisplay);
+    	  
+    // update .ui-slider-horizontal .ui-slider-handle
+    setScrollHandle(scrollbar, fDisplay);	
+}
+
+function addZoomEventHandlers(fDisplay) {
+	$('#plus'+fDisplay.index).mousedown(function(event){
+		fDisplay.minimumDisplay = true;
+	    zooming = 1;
+		zoom(fDisplay, $('#slider'+fDisplay.index), 1);	
+	});
+	
+	$('#plus'+fDisplay.index).mouseup(function(event){
+		fDisplay.minimumDisplay = false;
+		zooming = 0;
+		drawAll(fDisplay);
+	});
+
+	$('#minus'+fDisplay.index).mousedown(function(event){
+		fDisplay.minimumDisplay = true;
+	    zooming = 1;
+		zoom(fDisplay, $('#slider'+fDisplay.index), -1);	
+	});
+	
+	$('#minus'+fDisplay.index).mouseup(function(event){
+		fDisplay.minimumDisplay = false;
+		zooming = 0;
+		drawAll(fDisplay);
+	});	
+}
