d3697ce0e2aaa8b2a1dec936261b091007e1e597
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-07-19 14:22:14 +0000

    code for setting up different translation tables
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/javascript/web-artemis@14250 ee4ac58c-ac51-4696-9907-e4b3aa274f04

--- js/aminoacid.js
+++ js/aminoacid.js
@@ -1,5 +1,6 @@
+var translation_table = new Array();
 
-var codon_translation_array = new Array (
+var translation_table_default = new Array (
 		'F', 'F', 'L', 'L',
 	    'S', 'S', 'S', 'S',
 	    'Y', 'Y', '#', '+',
@@ -21,6 +22,143 @@ var codon_translation_array = new Array (
 	    'G', 'G', 'G', 'G'
 	  );
 
+var start_codons_1 = new Array ('ATG');
+
+// For the following Genetic Code tables the differences
+// from the Standard Code are given.
+
+//2. Vertebrate Mitochondrial Code
+translation_table[1] = new Array(2);
+translation_table[1][0] = new Array('AGA', 'AGG','ATA', 'TGA');
+translation_table[1][1] = new Array('*',   '*',  'M',   'W');
+var start_codons_2 = new Array ('ATG');
+
+// 3. Yeast Mitochondrial Code
+translation_table[2] = new Array(2);
+translation_table[2][0] = new Array('ATA', 'CTT', 'CTC', 'CTA', 'CTG', 'TGA');
+translation_table[2][1] = new Array('M',   'T',   'T',   'T',   'T',   'W');
+var start_codons_3 = new Array('ATA', 'ATG');
+
+// 4. Mold, Protozoan, and Coelenterate Mitochondrial Code and the
+//    Mycoplasma/Spiroplasma Code
+translation_table[3] = new Array(2);
+translation_table[3][0] = new Array('TGA');
+translation_table[3][1] = new Array('W');
+var start_codons_4 =  new Array('ATG');
+
+// 5. Invertebrate Mitochondrial Code
+translation_table[4] = new Array(2);
+translation_table[4][0] = new Array('AGA', 'AGG', 'ATA', 'TGA');
+translation_table[4][1] = new Array('S',   'S',   'M',   'W');
+var start_codons_5 = new Array('ATG', 'ATA', 'ATT');
+
+// 6. Ciliate, Dasycladacean and Hexamita Nuclear Code
+translation_table[5] = new Array(2);
+translation_table[5][0] = new Array('TAA', 'TAG');
+translation_table[5][1] = new Array('Q',   'Q');
+var start_codons_6 = new Array('ATG');
+
+// 9. Echinoderm and Flatworm Mitochondrial Code
+translation_table[8] = new Array(2);
+translation_table[8][0] = new Array('AAA', 'AGA', 'AGG', 'TGA');
+translation_table[8][1] = new Array('N',   'S',   'S',   'W');
+var start_codons_9 = new Array('ATG', 'GTG');
+
+// 10. Euplotid Nuclear Code
+translation_table[9] = new Array(2);
+translation_table[9][0] = new Array('TGA');
+translation_table[9][1] = new Array('C');
+var start_codons_10 = new Array('ATG');
+
+// 11. Bacterial and Plant Plastid 
+//translation_table_11 = 
+var start_codons_11 = new Array('ATG', 'GTG', 'TTG');
+
+// 12. Alternative Yeast Nuclear Code
+translation_table[11] = new Array(2);
+translation_table[11][0] = new Array('CTG');
+translation_table[11][1] = new Array('S');
+var start_codons_12 = new Array('CTG', 'ATG');
+
+// 13.  Ascidian Mitochondrial Code
+translation_table[12] = new Array(2);
+translation_table[12][0] = new Array('AGA', 'AGG', 'ATA', 'TGA');
+translation_table[12][1] = new Array('G',   'G',   'M',   'W');
+var start_codons_13 = new Array('ATG');
+
+// 14. Alternative Flatworm Mitochondrial Code
+translation_table[13] = new Array(2);
+translation_table[13][0] = new Array('AAA', 'AGA', 'AGG', 'TAA', 'TGA');
+translation_table[13][1] = new Array('N',   'S',   'S',   'Y',   'W');
+var start_codons_14 = new Array('ATG');
+
+// 15. Blepharisma
+translation_table[14] = new Array(2);
+translation_table[14][0] = new Array('TAG');
+translation_table[14][1] = new Array('Q');
+var start_codons_15 = new Array('ATG');
+
+// 16. Chlorophycean Mitochondrial
+translation_table[15] = new Array(2);
+translation_table[15][0] = new Array('TAG');
+translation_table[15][1] = new Array('L');
+var start_codons_16 = new Array('ATG');
+
+// 21. Trematode Mitochondrial
+translation_table[20] = new Array(2);
+translation_table[20][0] = new Array('TGA', 'ATA', 'AAA', 'AGA', 'AGG');
+translation_table[20][1] = new Array('W',   'M',   'N',   'S',   'S');
+var start_codons_21 = new Array('ATG', 'GTG');
+
+// 22. Scenedesmus obliquus mitochondrial
+translation_table[21] = new Array(2);
+translation_table[21][0] = new Array('TCA', 'TAG');
+translation_table[21][1] = new Array('*',   'L');
+var start_codons_22 = new Array('ATG');
+
+// 23. Thraustochytrium Mitochondrial
+translation_table[21] = new Array(2);
+translation_table[21][0] = new Array('TTA');
+translation_table[21][1] = new Array('*');
+var start_codons_23 = new Array('ATT', 'ATG', 'GTG');
+
+var codon_translation_array = translation_table_default;
+
+function setTranslationTable(tableIndex) {
+	debugLog("setTranslationTable "+tableIndex);
+	tableIndex--;
+	codon_translation_array = translation_table_default;
+	
+    for(var i = 0; i < translation_table[tableIndex][0].length; ++i) {
+      var cod_plus_aa = translation_table[tableIndex][0][i];
+      var codon_index = getIndexOfBase(cod_plus_aa.charAt(0)) * 16 +
+                        getIndexOfBase(cod_plus_aa.charAt(1)) * 4 +
+                        getIndexOfBase(cod_plus_aa.charAt(2));
+
+      debugLog(cod_plus_aa+" "+translation_table[tableIndex][1][i]+"  "+codon_index+"  "+
+    		  codon_translation_array[codon_index]);
+      
+      codon_translation_array[codon_index] = translation_table[tableIndex][1][i];
+    }
+}
+
+function getIndexOfBase(base) {
+  switch(base) {
+    case 'C':
+      return 1;
+    case 'A':
+      return 2;
+    case 'G':
+      return 3;
+    case 'T':
+    case 'U':
+      return 0;
+  }
+
+  return 4;
+}
+
+
 function getCodonTranslation(first_letter, second_letter, third_letter) {
   var first_index = getIndexOfBase(first_letter);
   if(first_index >= 4)
@@ -39,8 +177,7 @@ function getCodonTranslation(first_letter, second_letter, third_letter) {
 }
 
 function getIndexOfBase(base) { 
-  switch(base) 
-  {
+  switch(base) {
     case 'C':
       return 1;
     case 'A':
