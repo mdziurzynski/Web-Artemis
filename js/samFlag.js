1c150f954a25ed08cc4d650abfb77102936b859b
tjc <tjc@ee4ac58c-ac51-4696-9907-e4b3aa274f04>
2010-11-24 16:41:09 +0000

    handle sam flag filtering
    git-svn-id: svn+ssh://svn.internal.sanger.ac.uk/repos/svn/pathsoft/psu/trunk/genlib/javascript/web-artemis@15109 ee4ac58c-ac51-4696-9907-e4b3aa274f04

---
+++ js/samFlag.js
@@ -0,0 +1,54 @@
+var BAMFLAGS = [["Read paired", 0x1],
+			    ["Read mapped in proper pair", 0x2],
+			    ["Read unmapped", 0x4],
+			    ["Mate unmapped", 0x8],
+			    ["Read reverse strand", 0x10],
+			    ["Mate reverse strand", 0x20],
+			    ["First in pair", 0x40],
+			    ["Second in pair", 0x80],
+			    ["Not primary alignment", 0x100],
+			    ["Read fails platform/vendor quality checks", 0x200],
+			    ["Read is PCR or optical duplicate", 0x400]];
+
+function filterFlagsDisplay(fDisplay, thisBam) {
+	$("div#properties").html("<div id='bamFiler"+thisBam.bamId+"'></div>");
+
+    var filterTable = '<table>';
+    for(i=0; i<BAMFLAGS.length; i++) {
+    	if(BAMFLAGS[i][1] & thisBam.flag) {
+    		filterTable += 
+    			'<tr><td><input type="checkbox" name="cbFilter'+i+'" checked="checked" value="'+
+    			BAMFLAGS[i][1]+'"/>'+BAMFLAGS[i][0]+'</td></tr>';
+    	} else {
+    		filterTable += 
+    			'<tr><td><input type="checkbox" name="cbFilter'+i+'" value="'+
+    			BAMFLAGS[i][1]+'"/>'+BAMFLAGS[i][0]+'</td></tr>';
+    	}
+    }
+    filterTable +='</table>';
+    $("div#bamFiler"+thisBam.bamId).html(filterTable);
+
+
+    $("div#bamFiler"+thisBam.bamId).dialog({ height: 385 ,
+		width:450, position: 'center', title:'Filter',
+		close: function(event, ui) { $(this).remove(); },
+		buttons: {
+		'Filter': function() {
+			var cbs = $('input[name*=cbFilter]')
+			  .map(function() { return this; }) // get element
+			  .get(); // convert to instance of Array (optional)
+			var newFlagValue = 0;
+			for(i=0; i<cbs.length; i++) {
+				if($(cbs[i]).is(':checked')) {
+					newFlagValue |= BAMFLAGS[i][1];
+				}
+			}
+			thisBam.flag = newFlagValue;
+			drawBam(fDisplay, thisBam.bamId);
+			$(this).dialog('close');
+		},
+		Cancel: function() {
+			$(this).dialog('close');
+		}
+	}});
+}
