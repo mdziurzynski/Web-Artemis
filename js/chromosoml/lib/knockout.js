782f5a3e481d5af75469ef63ade1a2d97505e22e
Giles Velarde <gv1@sanger.ac.uk>
2011-10-31 16:27:26 +0000

    trying to fix what might be broken in IE7 9

--- js/chromosoml/lib/knockout.js
+++ js/chromosoml/lib/knockout.js
@@ -1733,6 +1733,9 @@ ko.templateRewriting = (function () {
         },
 
         memoizeBindingAttributeSyntax: function (htmlString, templateEngine) {
+            if( window['console'] !== undefined ){
+                console.log(htmlString);
+            }
             // temporary hack to make the gene page mostly work in IE7
             if (htmlString == undefined) {
                 return '';
